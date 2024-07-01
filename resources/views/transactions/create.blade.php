6941fad5ebb56220de9e6d41a2e387ec87cc39bc
WIRA <s160421134@student.ubaya.ac.id>
2024-07-01 18:14:47 +0700

    gbygb

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -2,42 +2,59 @@
 
 @section('content')
     <div class="container py-5">
-        <h1>Create Transaction</h1>
-        <form action="{{ route('transactions.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="user_id">User</label>
-                <select name="user_id" class="form-control" required>
-                    @foreach($users as $user)
-                        <option value="{{ $user->id }}">{{ $user->name }}</option>
-                    @endforeach
-                </select>
-            </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <h1 class="card-title text-center mb-4">Create Transaction</h1>
+                        <form action="{{ route('transactions.store') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="user_id">User</label>
+                                <select name="user_id" class="form-control" required>
+                                    @foreach($users as $user)
+                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-            <h3>Select Product</h3>
-            <div class="row">
-            
-                    <div class="col-md-6 mb-4">
-                        <div class="card shadow">
-                            <h5 class="card-title">Hotel </h5>
-                            <img src="https://ik.imagekit.io/tvlk/generic-asset/Ixf4aptF5N2Qdfmh4fGGYhTN274kJXuNMkUAzpL5HuD9jzSxIGG5kZNhhHY-p7nw/hotel/asset/10003464-49d01a697ba2d4b93bb3be559628b775.jpeg?_src=imagekit&tr=dpr-2,f-jpg,h-150,pr-true,q-100,w-375" class="card-img-top"  style="height: 200px; object-fit: cover;">
-                            <div class="card-body">
-                                <h5 class="card-title">(1x) Room</h5>
-                                <p class="card-text"><strong>Facility:</strong> suka</p>
+                            <h3>Select Product</h3>
+                            <div class="row">
+                                <div class="col-12 mb-4">
+                                    <div class="card shadow">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <img src="https://ik.imagekit.io/tvlk/generic-asset/Ixf4aptF5N2Qdfmh4fGGYhTN274kJXuNMkUAzpL5HuD9jzSxIGG5kZNhhHY-p7nw/hotel/asset/10003464-49d01a697ba2d4b93bb3be559628b775.jpeg?_src=imagekit&tr=dpr-2,f-jpg,h-150,pr-true,q-100,w-375" class="img-fluid rounded" alt="Hotel Image">
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <h5 class="card-title">Hotel Name</h5>
+                                                    <p class="card-text"><strong>Facility:</strong> Facility details here</p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <h6><strong>Total Rooms</strong></h6>
+                                                    <p class="card-text">1 Room(s)</p>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <h6><strong>Total Price</strong></h6>
+                                                    <p class="card-text">Rp. {{ number_format(1000000, 0, ',', '.') }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <hr class="m-0">
-                            <h6><strong>Total Rooms Price</strong></h6>
-                            <div class="card-body d-flex justify-content-between" >
-                                <p class="card-text"> 1 Room(s)</p>
-                                <p class="card-text"> Rp. {{ number_format(1000000, 0, ',', '.') }}</p>                               
+
+                            <div class="text-center mt-4">
+                                <button type="submit" class="btn btn-primary">Payment</button>
                             </div>
-                            
-                        </div>
+                        </form>
                     </div>
-               
+                </div>
             </div>
-
-            <button type="submit" class="btn btn-primary">Continous Pay,ent</button>
-        </form>
+        </div>
     </div>
 @endsection
